ba7e3e59964b1acc703fad8ae66075bf4cc8f055
Ioannis Tzanellis <Ioannis.Tzanellis@aimms.com>
2016-03-16 17:47:25 +0100

    gulpfile.js: Redefined default task to also apply babelify and changed destination dir

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ const babel = require("gulp-babel");
 const cache = require("gulp-cached");
 const eslint = require("gulp-eslint");
 
-const jsSources = ["src/*.js", "src/**/*.js"];
+const SOURCES = ["src/*.js", "src/**/*.js"];
+const DEST_DIR = "src-gen";
 
 gulp.task("babelify", () => {
-    gulp.src(jsSources)
+    gulp.src(SOURCES)
     // Use plumber to handle errors without terminating the pipe-chain
     .pipe(plumber())
     // Use gulp-cache to avoid re-transpiling files with no changes
@@ -15,18 +16,18 @@ gulp.task("babelify", () => {
     .pipe(babel({
         presets: ["es2015"]
     }))
-    .pipe(gulp.dest("es5-src"));
+    .pipe(gulp.dest(DEST_DIR));
 });
 
 gulp.task("lint", () => {
-    return gulp.src(jsSources)
+    return gulp.src(SOURCES)
         .pipe(eslint())
         .pipe(eslint.format())
-        .pipe(eslint.failAfterError);
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task("watch", () => {
-    gulp.watch(jsSources, ["babelify"]);
+    gulp.watch(SOURCES, ["babelify"]);
 });
 
-gulp.task("default", ["lint", "watch"]);
+gulp.task("default", ["babelify", "watch", "lint"]);
