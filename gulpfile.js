116604ede79716b073fd18f770d1cacf09660a2e
Ioannis Tzanellis <Ioannis.Tzanellis@aimms.com>
2016-03-17 17:55:03 +0100

    gulpfile.js: Created dev-start task which also accepts arguments

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const plumber = require("gulp-plumber");
 const babel = require("gulp-babel");
 const cache = require("gulp-cached");
 const eslint = require("gulp-eslint");
+const nodemon = require("gulp-nodemon");
 
 const SOURCES = ["src/*.js", "src/**/*.js"];
 const DEST_DIR = "src-gen";
+const MAIN = `${DEST_DIR}/app.js`;
 
 gulp.task("babelify", () => {
     gulp.src(SOURCES)
@@ -21,13 +23,21 @@ gulp.task("babelify", () => {
 
 gulp.task("lint", () => {
     return gulp.src(SOURCES)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task("watch", () => {
     gulp.watch(SOURCES, ["babelify"]);
 });
 
-gulp.task("default", ["babelify", "watch", "lint"]);
+gulp.task("nodemon", () => {
+    nodemon({
+        script: MAIN,
+        args:process.argv.splice(3, process.argv.length)
+    });
+});
+
+gulp.task("default", ["babelify", "lint"]);
+gulp.task("dev-start", ["babelify", "lint", "watch", "nodemon"]);
